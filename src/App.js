a0b9e9d4b3c292b05eba530655fd52752b43948e
mfelix5 <mikefelix@gmail.com>
2018-08-31 10:28:42 -0400

    added paginated search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const DEFAULT_QUERY = 'react'
+const DEFAULT_QUERY = ''
+const DEFAULT_HPP = '100'
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage='
 
 class App extends Component {
   constructor(props) {
@@ -22,11 +25,20 @@ class App extends Component {
   }
   
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+    
+    this.setState({ 
+      result: { hits: updatedHits, page } 
+    });
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
     .catch(error => error);
@@ -59,9 +71,7 @@ class App extends Component {
   
   render() {
     const { searchTerm, result } = this.state;
-    
-    if (!result) { return null; }
-    
+    const page = (result && result.page) || 0;    
     return (
       <div className="page">
         <div className="interactions">
@@ -74,12 +84,16 @@ class App extends Component {
           </Search>
         </div>
         { result  
-         ? <Table 
+         && <Table 
             list={result.hits}
             onDismiss={this.onDismiss}
-          />
-        : null  
+          />  
         }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page +1)}>
+            More
+            </Button>
+        </div>
       </div>
     );
   }
